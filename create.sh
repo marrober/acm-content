636d68194fe5024f3cd3edd4fd90f7897e312b4d
Mark Roberts <marrober@redhat.com>
2020-09-14 09:08:02 +0100

    Addition

---
+++ create.sh
@@ -0,0 +1,5 @@
+oc create -f acm/management/namespace.yaml 
+oc create -f acm/management/channel.yaml 
+oc create -f acm/management/subscription.yaml 
+oc create -f acm/management/application.yaml 
+oc create -f acm/management/placement-rule.yaml 
