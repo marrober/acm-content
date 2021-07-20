5d9c1af42013d6430c6d6af459968ca6e9fda3ff
Mark Roberts <marrober@redhat.com>
2021-07-20 11:15:01 +0100

    Update channel.yaml, config_placement_rule.yaml, and 12 more files...

--- create.sh
+++
@@ -1,5 +0,0 @@
-oc create -f acm/management/namespace.yaml 
-oc create -f acm/management/channel.yaml 
-oc create -f acm/management/subscription.yaml 
-oc create -f acm/management/application.yaml 
-oc create -f acm/management/placement-rule.yaml 
