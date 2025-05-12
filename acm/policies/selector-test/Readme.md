b477eda50122a9363ef48d49e023ea3de2d123d5
marrober <marrober@redhat.com>
2025-05-12 12:06:11 +0100

    update

---
+++ acm/policies/selector-test/Readme.md
@@ -0,0 +1,6 @@
+# Generate config maps of specific array data from Kubernetes resources
+
+## Get groovy.jaar
+
+wget -o /tmp/groovy.zip https://groovy.jfrog.io/artifactory/libs-release-local/org/apache/groovy/groovy/4.0.26/groovy-4.0.26.jar
+
--- acm/policies/selector-test/config-map-generator.groovy
+++ acm/policies/selector-test/config-map-generator.groovy
@@ -1,4 +1,5 @@
 import groovy.json.JsonSlurper
+import java.io.File
 
 /*def ocGetPodCommand = "oc get pod/my-quarkus-app-run-build-xb6q4-build-sign-image-pod -n  my-quarkus-app-dev -o json"
 
@@ -10,6 +11,7 @@ println proc.text
 println b.toString()
 */
 
+def namespace = "cm-1"
 
 
 String fileContents = new File("/home/marrober/temp/pod-data.json").text
@@ -23,11 +25,27 @@ slurpedData.spec.containers.each {  container ->
 
   //println(container.name)
   //println(container.image)
-  configMapData = "apiVersion: v1\nkind: ConfigMap\nmetadata:\n  name: " + container.name + "\n  namespace: default\ndata:\n"
+  configMapData = "apiVersion: v1\nkind: ConfigMap\nmetadata:\n  name: " + container.name + "\n  namespace: " + namespace + "\ndata:\n"
   configMapData += "  containerName: " + container.name + "\n"
   configMapData += "  containerImage: " + container.image + "\n"
 
   println(configMapData)
+
+  def newFile = new File("configmap.yaml")
+  newFile.createNewFile() 
+  newFile.text = configMapData
+
+  ocCreateConfigMapCmd = "oc apply --overwrite=true -f configmap.yaml"
+
+  println(ocCreateConfigMapCmd)
+
+  def proc = ocCreateConfigMapCmd.execute()
+  def b = new StringBuffer()
+  proc.consumeProcessErrorStream(b)
+
+  println proc.text
+  println b.toString()
+
   println("----------------------------------------------------------------------------------------------------------")
 }
 
